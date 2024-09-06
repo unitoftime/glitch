7ef8dea0b9bb60e01aa220bc7b05e024b85f15b2
Jacob <2606873+unitoftime@users.noreply.github.com>
2024-09-06 13:42:25 -0400

    Fix slice out of range bug

--- ui/widget.go
+++ ui/widget.go
@@ -203,8 +203,10 @@ func (resp *WidgetResp) recordTyped(str *string, cursorPosRet *int) {
 	} else if global.win.Repeated(glitch.KeyBackspace) {
 		if len(tStr) > 0 {
 			// tStr = tStr[:len(tStr)-1]
-			tStr = tStr[:cursorPos-1] + tStr[cursorPos:]
-			cursorPos--
+			if cursorPos > 0 {
+				tStr = tStr[:cursorPos-1] + tStr[cursorPos:]
+				cursorPos--
+			}
 		}
 	}
 
